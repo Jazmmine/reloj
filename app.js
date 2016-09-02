b210edfd813d7213ed29560284f79e51e1788ab0
Jazmmine <jazmine.angoma@gmail.com>
2016-09-02 17:49:48 -0500

    https://github.com/Jazmmine/reloj.git

---
+++ app.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function() {
+
+    function mueveReloj(){ 
+           momentoActual = new Date() ;
+           hora = momentoActual.getHours() ;
+           minuto = momentoActual.getMinutes() ;
+           segundo = momentoActual.getSeconds() ;
+
+           minuto = cero(minuto);
+           segundo = cero(segundo);
+
+           horaImprimible = hora + " : " + minuto + " : " +segundo ;
+           
+           var resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<p>" + horaImprimible + "</p>";
+
+        var resultadoArgentina = document.getElementById("resultadoArgentina");
+        resultadoArgentina.innerHTML = (hora+2) + " : " + minuto + " : " + segundo;
+
+           setTimeout(mueveReloj,1000) ;
+    }
+
+    function cero(i) {
+        if (i<10) { i="0" +i};
+        return i; 
+    }
+
+    mueveReloj();
+});
